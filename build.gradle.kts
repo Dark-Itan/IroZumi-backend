f18f7e74a3f7a4c7dc99b5758c56863af2e39a65
Dark-Itan <251242@ids.upchiapas.edu.mx>
2026-07-16 12:12:11 -0600

    Fix Dockerfile with fat JAR

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,12 +45,16 @@ kotlin {
     jvmToolchain(17)
 }
 
-tasks.withType<Jar> {
-    manifest {
-        attributes["Main-Class"] = "com.irozumi.ApplicationKt"
-    }
+tasks.register<Jar>("buildFatJar") {
+    group = "build"
+    archiveClassifier.set("all")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(sourceSets.main.get().output)
+    dependsOn(configurations.runtimeClasspath)
     from({
         configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
     })
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Main-Class"] = "com.irozumi.ApplicationKt"
+    }
 }
